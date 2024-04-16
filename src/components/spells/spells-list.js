8d4bd388e7d8e665ed9b4c84d009aadc1b264753
Alexander Langolf <sasha.langolf@gmail.com>
2024-04-16 20:20:40 +0100

    Modal continue

--- src/components/spells/spells-list.js
+++ src/components/spells/spells-list.js
@@ -1,3 +1,42 @@
+export const evaSpells = [
+  {
+    id: 'armour',
+    title: 'Броня Быстрого Течения',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/eva/1.png',
+  },
+  {
+    id: 'resist',
+    title: 'Sint minim do mollit',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше. Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/eva/2.png',
+  },
+  {
+    id: 'bonechill',
+    title: 'Commodo enim pariatur',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/eva/3.png',
+  },
+  {
+    id: 'arc',
+    title: 'Aliquip laborum duis',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/eva/4.png',
+  },
+  {
+    id: 'med',
+    title: 'Mollit qui nisi aute.',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/eva/5.png',
+  },
+  {
+    id: 'stomp',
+    title: 'Occaecat laborum excepteur',
+    text: 'Ipsum ea exercitation pariatur ad amet officia voluptate adipisicing ad. Tempor et deserunt mollit veniam consectetur. Sunt excepteur est id culpa minim cupidatat. Ea quis magna ipsum ullamco fugiat exercitation adipisicing dolore anim pariatur ullamco aliquip ipsum. Magna consectetur id quis laboris eiusmod sit in et velit pariatur veniam nisi esse. Laboris cupidatat elit officia esse.',
+    img: '/src/assets/images/spells/eva/6.png',
+  },
+];
+
 export const spiritSpells = [
   {
     id: 'armour',
@@ -8,7 +47,7 @@ export const spiritSpells = [
   {
     id: 'resist',
     title: 'Sint minim do mollit',
-    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше. Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
     img: '/src/assets/images/spells/spirit/2.png',
   },
   {
@@ -28,5 +67,77 @@ export const spiritSpells = [
     title: 'Mollit qui nisi aute.',
     text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
     img: '/src/assets/images/spells/spirit/5.png',
+  },
+];
+
+export const shilenSpells = [
+  {
+    id: 'armour',
+    title: 'Aliquip voluptate fugiat',
+    text: 'Aliquip nostrud laboris officia. Commodo et adipisicing labore sunt nisi ut proident consectetur laboris Lorem do officia ea qui quis. Et sit ad eiusmod eiusmod sunt culpa aliqua ex consequat tempor. Labore nulla aliqua et consectetur est magna et in elit ex deserunt elit Lorem elit.',
+    img: '/src/assets/images/spells/shilen/1.png',
+  },
+  {
+    id: 'resist',
+    title: 'Sint minim do mollit',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше. Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/shilen/2.png',
+  },
+  {
+    id: 'bonechill',
+    title: 'Commodo enim pariatur',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/shilen/3.png',
+  },
+  {
+    id: 'arc',
+    title: 'Aliquip laborum duis',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/shilen/4.png',
+  },
+  {
+    id: 'med',
+    title: 'Mollit qui nisi aute.',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/shilen/5.png',
+  },
+  {
+    id: 'stomp',
+    title: 'Occaecat laborum excepteur',
+    text: 'Ipsum ea exercitation pariatur ad amet officia voluptate adipisicing ad. Tempor et deserunt mollit veniam consectetur. Sunt excepteur est id culpa minim cupidatat. Ea quis magna ipsum ullamco fugiat exercitation adipisicing dolore anim pariatur ullamco aliquip ipsum. Magna consectetur id quis laboris eiusmod sit in et velit pariatur veniam nisi esse. Laboris cupidatat elit officia esse.',
+    img: '/src/assets/images/spells/shilen/6.png',
+  },
+];
+
+export const templarSpells = [
+  {
+    id: 'armour',
+    title: 'Броня Быстрого Течения',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/templar/1.png',
+  },
+  {
+    id: 'resist',
+    title: 'Sint minim do mollit',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше. Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/templar/2.png',
+  },
+  {
+    id: 'bonechill',
+    title: 'Commodo enim pariatur',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/templar/3.png',
+  },
+  {
+    id: 'arc',
+    title: 'Aliquip laborum duis',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/templar/4.png',
+  },
+  {
+    id: 'med',
+    title: 'Mollit qui nisi aute.',
+    text: 'Укрепите себя, черпая силу воды, и защитите себя сильными течениями. Увеличивает Сила Крит. Атк. физ. умений а также защищает от урона умений с 2 ударами и больше.',
+    img: '/src/assets/images/spells/templar/5.png',
   },
 ];
