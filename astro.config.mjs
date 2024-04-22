ef44b4bfbf839f393d569f07ccdc1ba9a64000a8
Alexander Langolf <sasha.langolf@gmail.com>
2024-04-22 22:29:52 +0100

    Skull fire animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import path from 'path';
-
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import webmanifest from 'astro-webmanifest';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 import { fileURLToPath } from 'url';
-
+import node from '@astrojs/node';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
   output: 'static',
   trailingSlash: 'never',
+  image: {
+    service: passthroughImageService(),
+  },
   build: {
     format: 'file',
     assetsPrefix: '/',
@@ -24,8 +28,16 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    webmanifest({
+      name: 'expanse-astro',
+      short_name: 'App',
+      description: 'Here is your app description',
+      start_url: '/',
+      theme_color: '#3367D6',
+      background_color: '#3367D6',
+      display: 'standalone',
+    }),
   ],
-
   vite: {
     resolve: {
       alias: {
@@ -33,4 +45,7 @@ export default defineConfig({
       },
     },
   },
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
